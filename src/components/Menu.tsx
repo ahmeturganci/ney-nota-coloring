cb4e36e8fd1ec1bb3fdfa0a0feeadf40ef6db628
ahmeturgancicls <aahmet.272@gmail.com>
2025-01-25 14:41:04 +0300

    header problem fixed

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -13,20 +13,22 @@ interface MenuProps {
 const Menu: React.FC<MenuProps> = ({ onClear, onExport }) => {
   return (
     <header className="header">
-      <nav className="nav-menu">
-        <div className="logo">Ney Nota</div>
-        <div className="nav-buttons">
-          <a href="#" onClick={(e) => { e.preventDefault(); onExport(); }}>
-            <FontAwesomeIcon icon={faFileExport} title="Export" />
-          </a>
-          <a href="#" onClick={(e) => { e.preventDefault(); onClear(); }}>
-            <FontAwesomeIcon icon={faTrash} title="Clear" />
-          </a>
-          <a href="https://github.com/ahmeturganci/ney-nota-coloring" target="_blank" rel="noopener noreferrer">
-            <FontAwesomeIcon icon={faGithub} title="GitHub" />
-          </a>
-        </div>
-      </nav>
+      <div className="header-container">
+        <nav className="nav-menu">
+          <div className="logo">Ney Nota</div>
+          <div className="nav-buttons">
+            <a href="#" onClick={(e) => { e.preventDefault(); onExport(); }}>
+              <FontAwesomeIcon icon={faFileExport} title="Export" />
+            </a>
+            <a href="#" onClick={(e) => { e.preventDefault(); onClear(); }}>
+              <FontAwesomeIcon icon={faTrash} title="Clear" />
+            </a>
+            <a href="https://github.com/ahmeturganci/ney-nota-coloring" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} title="GitHub" />
+            </a>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 };
